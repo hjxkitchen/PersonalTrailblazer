4e2bbbba80397321cfb877433e916a8a37d0a7ee
HJX <husseincollege@gmail.com>
2026-01-10 22:12:15 -0800

    build

--- server/index.ts
+++ server/index.ts
@@ -61,7 +61,7 @@ app.use((req, res, next) => {
     const port = Number(process.env.PORT) || 5000;
     server.listen({
       port,
-      host: "0.0.0.0",
+      host: "localhost",
     }, () => {
       log(`serving on port ${port}`);
     });
